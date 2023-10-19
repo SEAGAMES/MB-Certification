52637f4e78ce317c3a1623e6c917df9733e5d7d1
DevGame <t.seagames@hotmail.com>
2023-10-19 16:19:57 +0700

    add th eng

--- src/service/apiCreatePDF.js
+++ src/service/apiCreatePDF.js
@@ -29,12 +29,13 @@ const createPDF = async (PdfForm) => {
 };
 
 const certification_pdf = async (excel, form) => {
+  console.log(form)
   const docDefinition = {
     pageSize: 'A4',
     pageOrientation: 'landscape',
 
     info: {
-      title: 'Certificate',
+      title: 'Certificate-' + form.language,
       author: "Bam",
       subject: 'Certificate',
       keyword: 'cer'
@@ -55,13 +56,10 @@ const certification_pdf = async (excel, form) => {
       icon: { font: 'Fontello' }
 
     },
-    watermark: { text: 'ตัวอย่าง', color: 'blue', opacity: 0.2, fontSize: 50, bold: false, italics: false },
+    //watermark: { text: 'ตัวอย่าง', color: 'blue', opacity: 0.2, fontSize: 50, bold: false, italics: false },
     content: run(excel, form),
 
     images: {
-    //   // logo: mulogo.mulogo,
-    //   // sign: mulogo.sign,
-      // footer: mulogo.footer,
       logo: imgFromBase64.logo_mu,
       sign: imgFromBase64.mysign,
       footer: imgFromBase64.myfoot
@@ -81,6 +79,7 @@ const certification_pdf = async (excel, form) => {
 
 function run(excel, form) {
   let s = []
+  console.log(form.add_name.length)
   excel.forEach((e, index) => {
     s.push(
       {
@@ -93,43 +92,143 @@ function run(excel, form) {
         absolutePosition: { x: 590, y: 40 }
       },
 
-      { qr: form.pj_code + String(index + 1).padStart(4, '0') , fit: '60', absolutePosition: { x: 710, y: 60 }},
+      { qr: form.pj_code + String(index + 1).padStart(4, '0'), fit: '60', absolutePosition: { x: 710, y: 60 } },
+
+      (form.language === "Eng") ? { text: 'Institute of  Molecular Biosciences', margin: [5, 8, 0, 0], color: '#1565C0', fontSize: 22 } : {},
+      (form.language === "Eng") ? { text: 'Mahidol University ', margin: [5, -16, 0, 0], color: '#1565C0', fontSize: 22 } : {},
+
+      (form.language === "TH") ? { text: 'เกียรติบัตรนี้ให้ไว้เพื่อแสดงว่า', margin: [5, 16, 0, 0] } : { text: 'This is to certify that', margin: [5, -3, 0, 0] },
 
       {
-        text: 'เกียรติบัตรนี้ให้ไว้เพื่อแสดงว่า', margin: [5, 16]
+        text: e.prefix + ' ' + e.name, color: '#0D47A1', fontSize: 28, margin: [5, -3, 0, 0]
       },
-      {
-        text: e.prefix + '' + e.name, color: '#1206AA', fontSize: 26
-      },
-      {
+
+      (form.language === "TH") ? {
         text: 'เข้าอบรมเชิงปฏิบัติการเรื่อง '
-      },
-      { text: "“" + form.pj_name + "”", color: '#1206AA', fontSize: 24 },
-      { text: form.date },
-      {
-        text: 'ณ สถาบันชีววิทยาศาสตร์โมเลกุล มหาวิทยาลัยมหิดล'
-      },
-      (form.sign) ? {
-        image: 'sign',
-        width: 250,
-        height: 80,
+      } : { text: 'participated in the practical training entitled', margin: [5, -3, 0, 0] },
 
+      (form.language === "TH") ? { text: "“" + form.pj_name + "”", color: '#1565C0', fontSize: 24 } : { text: form.pj_name, color: '#1565C0', fontSize: 24, margin: [5, -3, 0, 0] },
+      { text: form.date },
+
+      (form.language === "TH") ? {
+        text: 'ณ สถาบันชีววิทยาศาสตร์โมเลกุล มหาวิทยาลัยมหิดล'
       } : {},
+
+      // 2 ลายเซ็น
+      (form.two_sign) ?
+      // 2 ลายเซ็น เป็น TH
+        (form.language === "TH") ? (form.sign) ? // 2 ลายเซ็น เป็น TH มี ผอ. เซ็น
+        {
+          columns: [
+            {
+              image: 'sign',
+              width: 250,
+              height: 80,
+              absolutePosition: { x: -200, y: 400 }
+            },
+            {
+              text: '(............................................)',
+              absolutePosition: { x: 335, y: 450 }
+            }
+          ]
+        } : { // 2 ลายเซ็น เป็น TH ไม่มี ผอ. เซ็น
+          columns: [
+            {
+              text: '(............................................)',
+              absolutePosition: { x: -205, y: 450 }
+            },
+            {
+              text: '(............................................)',
+              absolutePosition: { x: 335, y: 450 }
+            }
+          ]
+        } : (form.sign) ? { // 2 ลายเซ็น เป็น ENG ผอ. เซ็น
+          columns: [
+            {
+              image: 'sign',
+              width: 250,
+              height: 80,
+              absolutePosition: { x: -200, y: 410 }
+            },
+            {
+              text: '(............................................)',
+              absolutePosition: { x: 335, y: 455 }
+            }
+          ],     
+        } 
+        : { // 2 ลายเซ็น เป็น TH ผอ . ไม่เซ็น
+          columns: [
+            {
+              text: '(............................................)',
+              absolutePosition: { x: -200, y: 455 }
+            },
+            {
+              text: '(............................................)',
+              absolutePosition: { x: 335, y: 455 }
+            }
+          ]
+        } : { 
+          
+
+        },
       {
-        text: 'ศาสตราจารย์ ดร. นายแพทย์นรัตถพล  เจริญพันธุ์ \n ผู้อำนวยการสถาบันชีววิทยาศาสตร์โมเลกุล', fontSize: 12, bolditalics: true,
-        absolutePosition: { x: 40, y: 490 }
+        alignment: 'justify',
+
+        columns: [
+          (form.language === "TH") ? {
+            text: 'ศาสตราจารย์ ดร. นายแพทย์นรัตถพล  เจริญพันธุ์', fontSize: 12, bolditalics: true,
+            absolutePosition: { x: 183, y: 490 }
+          } : {
+            text: 'Professor Narattaphol Charoenphandhu, M.D., Ph.D.', fontSize: 12, bolditalics: true,
+            absolutePosition: { x: 170, y: 500 }
+          },
+          {
+            text: `${form.add_name}`, fontSize: 12, bolditalics: true,
+            absolutePosition: { x: 520, y: 500 }
+            // absolutePosition: form.add_name.length <= 30 ? { x: 520 - countDivisions(form.add_name.length), y: 490 } : { x: 450, y: 490 }
+          },
+        ]// obj.reason.length === 2 ? [0, 0, 0, -20]
       },
       {
-          image: 'footer',
-          width: 842,
-          height: 50,
-          absolutePosition: { x: 40, y: 550 }, pageBreak: 'after'
+        alignment: 'justify',
+
+        columns: [
+          (form.language === "TH") ? {
+            text: 'ผู้อำนวยการสถาบันชีววิทยาศาสตร์โมเลกุล', fontSize: 12, bolditalics: true,
+            absolutePosition: { x: 195, y: 520 }
+          } : { text: 'Director', fontSize: 12, bolditalics: true, absolutePosition: { x: 280, y: 520 } },
+          {
+            text: `${form.position}`, fontSize: 12, bolditalics: true,
+            absolutePosition: { x: 475, y: 520 }
+            // absolutePosition: form.add_name.length <= 30 ? { x: 520 - countDivisions(form.add_name.length), y: 490 } : { x: 450, y: 490 }
+          },
+        ]// obj.reason.length === 2 ? [0, 0, 0, -20]
+      },
+      {
+        image: 'footer',
+        width: 842,
+        height: 50,
+        absolutePosition: { x: 40, y: 550 }, pageBreak: 'after'
       },
 
     )
   })
   return s
 }
+
+// function countDivisions(dividend) {
+//   console.log('length : ', dividend)
+//   let divisor = 2
+
+//     dividend = dividend / divisor;
+
+
+//   console.log('dividend : ', dividend)
+//   console.log(dividend + 15)
+//   return dividend + 15 
+// }
+
+
 
 export default {
   createPDF,
--- src/views/Certification-PDF.vue
+++ src/views/Certification-PDF.vue
@@ -1,43 +1,82 @@
 <template>
   <v-row justify="center">
     <v-col cols="12" sm="10" md="8" lg="6">
-      <v-card ref="form" height="300">
+      <v-card ref="form" height="260">
         <v-card-text>
-          <v-text-field
-            label="ชื่อโครงการ"
-            v-model="form.pj_name"
-          ></v-text-field>
-          <v-text-field
-            label="รหัสโครงการ"
-            id="pj_name"
-            v-model="form.pj_code"
-            @input="form.pj_code = $event.target.value.toUpperCase()"
-          ></v-text-field>
-          <v-text-field label="วันที่" v-model="form.date"></v-text-field>
+          <v-row
+            ><v-col
+              ><v-text-field
+                label="ชื่อโครงการ"
+                v-model="form.pj_name"
+              ></v-text-field></v-col
+            ><v-col
+              ><v-row
+                ><v-col>
+                  <v-switch
+                    v-model="form.language"
+                    hide-details
+                    true-value="Eng"
+                    false-value="TH"
+                    color="indigo"
+                    value="indigo"
+                    :label="`ฟอร์ม : ${form.language}`"
+                  ></v-switch> </v-col
+                ><v-col
+                  ><v-checkbox
+                    label="ใส่ลายเซ็น ผอ."
+                    v-model="form.sign"
+                    color="indigo"
+                    value="indigo"
+                    hide-details
+                  ></v-checkbox></v-col
+                ><v-col
+                  ><v-checkbox
+                    label="2 ลายเซ็น"
+                    v-model="form.two_sign"
+                    color="indigo-darken-3"
+                    value="indigo-darken-3"
+                    hide-details
+                  ></v-checkbox></v-col></v-row></v-col
+          ></v-row>
+          <v-row class="mt-n8">
+            <v-col
+              ><v-text-field
+                label="รหัสโครงการ"
+                id="pj_name"
+                v-model="form.pj_code"
+                @input="form.pj_code = $event.target.value.toUpperCase()"
+              ></v-text-field
+            ></v-col>
+            <v-col
+              ><v-text-field
+                label="ชื่อ"
+                v-model="form.add_name"
+                :disabled="!form.two_sign"
+              ></v-text-field
+            ></v-col>
+          </v-row>
+          <v-row class="mt-n8"
+            ><v-col
+              ><v-text-field
+                label="วันที่"
+                v-model="form.date"
+              ></v-text-field></v-col
+            ><v-col
+              ><v-text-field
+                label="ตำแหน่ง"
+                v-model="form.position"
+                :disabled="!form.two_sign"
+              ></v-text-field></v-col
+          ></v-row>
           <v-row>
-            <v-checkbox
-              label="ใส่ลายเซ็น ผอ."
-              v-model="form.sign"
-              color="indigo"
-              value="indigo"
-              hide-details
-            ></v-checkbox>
-            <v-checkbox
-              label="2 ลายเซ็น"
-              color="indigo-darken-3"
-              value="indigo-darken-3"
-              hide-details
-            ></v-checkbox>
-
             <input
-              class="mt-4"
+              class="mt-n5 ml-3"
               type="file"
               @change.left="add_file()"
               @click="this.$refs.myFiles.value = null"
               ref="myFiles"
               accept=".xls, .xlsx, .csv"
-            />
-          </v-row>
+          /></v-row>
         </v-card-text>
         <v-divider class="mt-12"></v-divider>
       </v-card>
@@ -63,9 +102,12 @@ export default {
         pj_name: "Molecular biology for life and medicine",
         pj_code: "2023-PAR-",
         date: "ระหว่างวันที่ 2-4 ตุลาคม 2566",
-        sign: false,
+        add_name: "อาจารย์ ธนกฤต นิ่มนวล",
+        position: "ผู้อำนวยการสถาบันชีววิทยาศาสตร์โมเลกุล",
+        language: "Eng",
+        sign: true,
+        two_sign: true,
       },
-      show: false,
       base_64: null,
       excel_array: null,
       preview: false,
@@ -97,8 +139,7 @@ export default {
         iframe.style.width = "770px"; // เปลี่ยนเป็นค่าที่คุณต้องการ
         iframe.style.height = "600px"; // เปลี่ยนเป็นค่าที่คุณต้องการ
       });
-      this.preview = true
-      this.show = true;
+      this.preview = true;
     },
   },
   watch: {
@@ -108,8 +149,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-embed {
-  height: 70vh;
-}
-</style>
